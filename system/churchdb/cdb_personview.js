644e1c23e8e936cf46cf4d5736d26245e1c378de
churchtools <jmrauen@churchtools.de>
2015-02-07 18:50:24 +0100

    (CDB) Optimize date until filter, add one day

--- system/churchdb/cdb_personview.js
+++ system/churchdb/cdb_personview.js
@@ -2192,6 +2192,8 @@ PersonView.prototype.checkFilter = function(a) {
       if (a[filter["filterDates"]]!=null) {
         flt=a[filter["filterDates"]].toDateEn();
         src=filter["dateBefore"].toDateDe();
+        src.addDays(1); src.setSeconds(src.getSeconds()-1);
+        console.log(src);
         if (filter["dateIgnoreYear"]!=null) {
           flt.setFullYear(2000);
           src.setFullYear(2000);
